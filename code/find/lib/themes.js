1ad255a2b278a34b4d543ca0ee4fd74d88661843
Paul Doherty <paulrobertdoherty@gmail.com>
2019-04-11 09:18:11 -0700

    Made themes.js a Map object

--- code/find/lib/themes.js
+++ code/find/lib/themes.js
@@ -1,107 +1,149 @@
-module.exports = [
-  {
-    "id": 38,
-    "name": "Open world",
-    "url": "https://www.igdb.com/themes/open-world"
-  },
-  {
-    "id": 19,
-    "name": "Horror",
-    "url": "https://www.igdb.com/themes/horror"
-  },
-  {
-    "id": 39,
-    "name": "Warfare",
-    "url": "https://www.igdb.com/themes/warfare"
-  },
-  {
-    "id": 32,
-    "name": "Non-fiction",
-    "url": "https://www.igdb.com/themes/non-fiction"
-  },
-  {
-    "id": 1,
-    "name": "Action",
-    "url": "https://www.igdb.com/themes/action"
-  },
-  {
-    "id": 18,
-    "name": "Science fiction",
-    "url": "https://www.igdb.com/themes/science-fiction"
-  },
-  {
-    "id": 23,
-    "name": "Stealth",
-    "url": "https://www.igdb.com/themes/stealth"
-  },
-  {
-    "id": 41,
-    "name": "4X (explore, expand, exploit, and exterminate)",
-    "url": "https://www.igdb.com/themes/4x-explore-expand-exploit-and-exterminate"
-  },
-  {
-    "id": 31,
-    "name": "Drama",
-    "url": "https://www.igdb.com/themes/drama"
-  },
-  {
-    "id": 22,
-    "name": "Historical",
-    "url": "https://www.igdb.com/themes/historical"
-  },
+module.exports = new Map([
+  [
+    38,
     {
-    "id": 28,
-    "name": "Business",
-    "url": "https://www.igdb.com/themes/business"
-  },
-  {
-    "id": 35,
-    "name": "Kids",
-    "url": "https://www.igdb.com/themes/kids"
-  },
-  {
-    "id": 42,
-    "name": "Erotic",
-    "url": "https://www.igdb.com/themes/erotic"
-  },
-  {
-    "id": 33,
-    "name": "Sandbox",
-    "url": "https://www.igdb.com/themes/sandbox"
-  },
-  {
-    "id": 40,
-    "name": "Party",
-    "url": "https://www.igdb.com/themes/party"
-  },
-  {
-    "id": 17,
-    "name": "Fantasy",
-    "url": "https://www.igdb.com/themes/fantasy"
-  },
-  {
-    "id": 43,
-    "name": "Mystery",
-    "url": "https://www.igdb.com/themes/mystery"
-  },
-  {
-    "id": 20,
-    "name": "Thriller",
-    "url": "https://www.igdb.com/themes/thriller"
-  },
-  {
-    "id": 21,
-    "name": "Survival",
-    "url": "https://www.igdb.com/themes/survival"
-  },
-  {
-    "id": 34,
-    "name": "Educational",
-    "url": "https://www.igdb.com/themes/educational"
-  },
-  {
-    "id": 27,
-    "name": "Comedy",
-    "url": "https://www.igdb.com/themes/comedy"
-  }
-]
+    	name: "Open world",
+    	url: "https://www.igdb.com/themes/open-world"
+    }
+  ],
+  [
+    19,
+    {
+    	name: "Horror",
+    	url: "https://www.igdb.com/themes/horror"
+    }
+  ],
+  [
+    39,
+    {
+    	name: "Warfare",
+    	url: "https://www.igdb.com/themes/warfare"
+    }
+  ],
+  [
+    32,
+    {
+    	name: "Non-fiction",
+    	url: "https://www.igdb.com/themes/non-fiction"
+    }
+  ],
+  [
+    1,
+    {
+    	name: "Action",
+    	url: "https://www.igdb.com/themes/action"
+    }
+  ],
+  [
+    18,
+    {
+    	name: "Science fiction",
+    	url: "https://www.igdb.com/themes/science-fiction"
+    }
+  ],
+  [
+    23,
+    {
+    	name: "Stealth",
+    	url: "https://www.igdb.com/themes/stealth"
+   	}
+  ],
+  [
+    41,
+    {
+    	name: "4X (explore, expand, exploit, and exterminate)",
+    	url: "https://www.igdb.com/themes/4x-explore-expand-exploit-and-exterminate"
+    }
+  ],
+  [
+    31,
+    {
+    	name: "Drama",
+    	url: "https://www.igdb.com/themes/drama"
+    }
+  ],
+  [
+    22,
+    {
+    	name: "Historical",
+    	url: "https://www.igdb.com/themes/historical"
+    }
+  ],
+    [
+    28,
+    {
+    	name: "Business",
+    	url: "https://www.igdb.com/themes/business"
+    }
+  ],
+  [
+    35,
+    {
+    	name: "Kids",
+    	url: "https://www.igdb.com/themes/kids"
+    }
+  ],
+  [
+    42,
+    {
+    	name: "Erotic",
+    	url: "https://www.igdb.com/themes/erotic"
+    }
+  ],
+  [
+    33,
+    {
+    	name: "Sandbox",
+    	url: "https://www.igdb.com/themes/sandbox"
+    }
+  ],
+  [
+    40,
+    {
+    	name: "Party",
+    	url: "https://www.igdb.com/themes/party"
+    }
+  ],
+  [
+    17,
+    {
+    	name: "Fantasy",
+    	url: "https://www.igdb.com/themes/fantasy"
+    }
+  ],
+  [
+    43,
+    {
+    	name: "Mystery",
+    	url: "https://www.igdb.com/themes/mystery"
+    }
+  ],
+  [
+    20,
+    {
+    	name: "Thriller",
+    	url: "https://www.igdb.com/themes/thriller"
+	}
+  ],
+  [
+    21,
+    {
+    	name: "Survival",
+    	url: "https://www.igdb.com/themes/survival"
+    }
+  ],
+  [
+    34,
+    {
+    	name: "Educational",
+    	url: "https://www.igdb.com/themes/educational"
+    }
+  ],
+  [
+    27,
+    {
+    	name: "Comedy",
+    	url: "https://www.igdb.com/themes/comedy"
+    }
+  ]
+]);
