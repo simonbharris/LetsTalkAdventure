7679786d438211e5e299611c744a532afe6130a0
Simon Harris <simonbharris@gmail.com>
2019-04-11 19:06:14 -0700

    Can not search on any one field of: Name, genre, theme, or platform.

--- code/find/FindGames.js
+++ code/find/FindGames.js
@@ -2,9 +2,9 @@ var http = require('http')
 var config = require('config')
 var textLib = require('textLib')
 var console = require('console')
-const platforms = require("./lib/platforms.js")
-const themes = require("./lib/themes.js")
-const genres = require("./lib/genres.js")
+const platformsLib = require("./lib/platformsLib.js")
+const themesLib = require("./lib/themesLib.js")
+const genresLib = require("./lib/genresLib.js")
 
 function pairIds(ids, group) {
   var result = [];
@@ -25,44 +25,47 @@ function convertToGames(apiObj)
     apiObj.forEach(function(o) {
     result.push({
       name: o.name,
-      platforms: pairIds(o.platforms, platforms),
-      themes: pairIds(o.themes, themes),
-      genres: pairIds(o.genres, genres)
+      platforms: pairIds(o.platforms, platformsLib),
+      themes: pairIds(o.themes, themesLib),
+      genres: pairIds(o.genres, genresLib),
     })
+      console.log("Pushing game with: " +o.platforms  + " " +  o.themes, + " " +   o.genres);
   });
   return result;
 }
 
-// - May need to switch to Enum or change from fuzzymatch;
-// Looking for "PlayStation" will return "PlayStation", "Playstation 2", "PlayStation portable"... etc.
-function getPlatformID(userInput) {
+function getEnumID(userInput, group)
+{
   var result = [];
-  platforms.forEach(function (platform) {
-    if (textLib.fuzzyMatch(platform[1].name, userInput.toString()))
-      result.push(platform[0]);
+  group.forEach(function (g) {
+    if (g[1].name == userInput.toString()) {
+      console.log("Adding" + g[0]);
+      result.push(g[0]);
+    }
+    console.log(g.toString() + " vs " + userInput );
   });
   return result;
 }
 
-function getThemeID(userInput) {
-  var result = [];
-  console.log("User Input: " + userInput.toString() + " vs " + themes[0][1].name);
-  themes.forEach(function (theme) {
-    if (theme[1].name == userInput.toString())
-      result.push(theme[0]);
-    console.log(theme);
-  });
-  return result;
+function simpleFieldSearch(fieldName, IDs, options)
+{
+    var apiObj = JSON.parse(
+                    http.postUrl(config.get('api.url') + "games/",
+                    "fields name, themes, platforms, genres;" + "where "+ fieldName +" = (" + IDs + ");",
+                    options))
+    return convertToGames(apiObj);
+  
 }
 
-module.exports.function = function findGames (name, platforms, themes) {
+module.exports.function = function findGames (name, platforms, themes, genres) {
   var options = {}
   options.headers = {
     "user-key": config.get('api.key')
   }
-  console.debug("Inside FindGames");
+  console.log("In findGames");
   // If name is supplied
   if (name) {
+    console.log("Name-only Route");
     var apiObj = JSON.parse(
                     http.postUrl(config.get('api.url') + "games/",
                     "fields name, themes, platforms, genres;" + "search \"" + name + "\";",
@@ -70,23 +73,29 @@ module.exports.function = function findGames (name, platforms, themes) {
     return convertToGames(apiObj);
   }
   
-  // if (themes) {
-  //   var platformIds = getThemeID(themes)
-  //   var apiObj = JSON.parse(
-  //                   http.postUrl(config.get('api.url') + "games/",
-  //                   "fields name, themes, platforms, genres;" + "where platforms = (" + platformIds + ");",
-  //                   options))
-  //   return convertToGames(apiObj);
-  // }
+     
+  if (themes) {
+    console.log("Theme-only Route");
+    var themeIds = getEnumID(themes, themesLib)
+    var games = simpleFieldSearch("themes", themeIds, options);
+    return games;
+  } 
+  
+  if (genres) {
+    console.log("Genre-only route");
+    var genreIds = getEnumID(genres, genresLib);
+    var games = simpleFieldSearch("genres", genreIds, options);
+    return games;
+  }
+
   
   if (platforms) {
-    var platformIds = getPlatformID(platforms)
-    var apiObj = JSON.parse(
-                    http.postUrl(config.get('api.url') + "games/",
-                    "fields name, themes, platforms, genres;" + "where platforms = (" + platformIds + ");",
-                    options))
-    return convertToGames(apiObj);
+    console.log("Platform-only Route");
+    var platformIds = getEnumID(platforms, platformsLib)
+    var games = simpleFieldSearch("platforms", platformIds, options);
+    return games;
   }
+
   
   var apiObj = JSON.parse(http.postUrl(config.get('api.url') + "games/", "fields name, themes, platforms, genres;", options))
   var result = convertToGames(apiObj);  
